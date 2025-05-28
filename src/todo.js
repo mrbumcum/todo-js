fb3bfbc01bcbf1ef8ad2fab68a270a6a65422b56
shucksow <102425362+shucksow@users.noreply.github.com>
2025-05-28 12:53:57 -0400

    feat: Add setters

--- src/todo.js
+++ src/todo.js
@@ -7,4 +7,25 @@ class TodoItem {
         this.completed = false;
     }
 
+
+    get title() {
+        return this.title;
+    }
+
+    get tags() {
+        return this.tags;
+    }
+
+    get notes() {
+        return this.notes;
+    }
+
+    get priority() {
+        return this.priority;
+    }
+
+    get completed() {
+        return this.completed;
+    }
+
 }
