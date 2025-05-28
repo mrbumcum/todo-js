94f3a32ac247099eb03ed294e6782b4586393d31
shucksow <102425362+shucksow@users.noreply.github.com>
2025-05-28 12:53:39 -0400

    feat: Initialize ToDoItem

---
+++ src/todo.js
@@ -0,0 +1,10 @@
+class TodoItem {
+    constructor(title, tags = [], notes = "", priority = "normal") {
+        this.title = this.title;
+        this.tags = this.tags;
+        this.notes = this.notes;
+        this.priority = this.priority;
+        this.completed = false;
+    }
+
+}
