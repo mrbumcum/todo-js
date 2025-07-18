64c4eb62370e69f8b40c1c3a1c5c167e95c8d731
shucksow <102425362+shucksow@users.noreply.github.com>
2025-07-18 13:55:15 -0400

    refactor: remove all

--- src/index.js
+++ src/index.js
@@ -1,37 +1,3 @@
-import { updateTodos, updateProjects, openProjectCreation, openTodoCreation, closeTodoCreation, getTodoFormData } from "./dom";
-import { Project } from "./project";
-
-const defaultProject = new Project("Inbox");
-let projectList = [defaultProject];
-let currentProject = projectList[0];
-
-// TODO: Add event listeners for project creation and todo creation
-document.addEventListener('DOMContentLoaded', () => {
-    updateTodos(currentProject);
-    openProjectCreation();
-    openTodoCreation();
-    closeTodoCreation();
-    updateProjects(projectList);
-    getTodoFormData(currentProject); 
-    projectCreation(projectList);
-    todoCreation(currentProject);
-})
-
-function switchProject (newProjectName) {
-    currentProject = projectList.find(project => project.name === newProjectName);
-    updateTodos(currentProject);
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+ 
