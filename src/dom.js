6dbdff3448c6b930a987c2ce857dca6c9b481f23
shucksow <102425362+shucksow@users.noreply.github.com>
2025-07-19 18:56:53 -0400

    feat: init function setup

--- src/dom.js
+++ src/dom.js
@@ -1,2 +1,59 @@
 import { Todo } from "./todo.js";
-import { Project  } from "./project.js";
+import { Project } from "./project.js";
+
+function clear(domElement) {
+    while ( domElement.hasChildNodes() ) {
+        domElement.removeChild(domElement.firstChild);
+    }
+}
+// TODO: Add CRUD for TODO
+export function toggleProjectMenu() {
+    return null;
+};
+
+export function getProjectInformation() {
+    return null;
+};
+
+export function createProject() {
+    return null;
+};
+
+export function deleteProject() {
+    return null;
+}
+
+export function updateProjectList(currentProject) {
+    return null;
+};
+
+// TODO: Add CRUD for PROJECT obj
+
+export function toggleCreateTodoMenu() {
+    return null;
+}
+
+export function getTodoInformation() {
+    return null;
+}
+
+export function updateTodoList(currentProject) {
+    return null;
+}
+
+export function createTodo() {
+    return null;
+}
+
+export function deleteTodo() {
+    return null;
+}
+
+
+
+
+
+
+
+
+
