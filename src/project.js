5a3a9fad5be7def086c1339aac2e6ee41e452500
shucksow <102425362+shucksow@users.noreply.github.com>
2025-07-21 16:08:48 -0400

    fix(project): use backing properties to avoid infinite recursion in getters/setters

--- src/project.js
+++ src/project.js
@@ -1,37 +1,37 @@
 export class Project {
     constructor(title, desc = "") {
-        this.title = title;
-        this.desc = desc;
-        this.todos = [];
-        this.id = crypto.randomUUID();
+        this._title = title;
+        this._desc = desc;
+        this._todos = [];
+        this._id = crypto.randomUUID();
     }
 
     get title() {
-        return this.title;
+        return this._title;
     }
 
     get desc() {
-        return this.desc;
+        return this._desc;
     }
 
     get todos() {
-        return this.todos;
+        return this._todos;
     }
 
     get id() {
-        return this.id;
+        return this._id;
     }
 
     addTodo(todo) {
-        this.todos.push(todo);
+        this._todos.push(todo);
     }
 
     set title(title) {
-        this.title = title;
+        this._title = title;
     }
 
     set desc(desc) {
-        this.desc = desc;
+        this._desc = desc;
     }
 }
 
